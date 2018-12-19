0e1ff1400419a99f092b5e3b4069c0bc4bc336d1
Wendi Chen <epanistamai.proles@gmail.com>
2018-12-19 22:44:43 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,38 @@
+class Song
+  attr_accessor :name, :artist, :genre
+  @@count = 0
+  @@genres = []
+  @@artists = []
+      def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @@artists << @artist
+        @genre = genre
+        @@genres << @genre
+        @@count += 1
+      end
+  
+  
+      def self.count 
+          return @@count
+      end
+  
+      def self.genres
+        a = 0
+          if @@genres.select{ |e| ary.count(e) > 1 }.uniq
+              @@genres.delete(s_gen)
+          end
+        return @@genres
+      end
+      
+      def self.artists
+         @@artists.detect do |s_art| 
+            @@artists.delete(s_art) if a.count(s_art) > 1 
+              
+              
+          end
+          
+        return @@artists
+      end
+  
+end
